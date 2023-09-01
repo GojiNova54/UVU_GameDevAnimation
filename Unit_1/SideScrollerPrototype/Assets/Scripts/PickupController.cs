20fc56313572ed207ac7643843e7bc62da42cada
Sean Anderson <nova7prospekt@gmail.com>
2023-09-01 10:33:54 -0600

    Added sound to player when picking up objects

--- Unit_1/SideScrollerPrototype/Assets/Scripts/PickupController.cs
+++ Unit_1/SideScrollerPrototype/Assets/Scripts/PickupController.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class PickupController : MonoBehaviour
 {
-    
+    private AudioSource pickupAudioSource; // Reference to the pickup's AudioSource
+    private Renderer pickupRenderer; // Reference to the pickup's Renderer
+
+    private void Start()
+    {
+        // Get the AudioSource component attached to the pickup
+        pickupAudioSource = GetComponent<AudioSource>();
+
+        // Get the Renderer component attached to the pickup
+        pickupRenderer = GetComponent<Renderer>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,8 +24,28 @@ public class PickupController : MonoBehaviour
 
     private void CollectPickup()
     {
-        // Implement any behavior you want when the pickup is collected
-        // For example, you can trigger an effect, update player's stats, etc.
-        Destroy(gameObject); // Destroy the pickup object
+        // Play the pickup sound from the pickup's AudioSource
+        if (pickupAudioSource != null)
+        {
+            pickupAudioSource.Play();
+        }
+
+        // Disable the Renderer component to make the object invisible
+        if (pickupRenderer != null)
+        {
+            pickupRenderer.enabled = false;
+        }
+
+        // Delay the destruction of the pickup object to allow the audio to finish playing
+        StartCoroutine(DestroyPickupWithDelay());
+    }
+
+    private System.Collections.IEnumerator DestroyPickupWithDelay()
+    {
+        // Wait for the duration of the audio clip before destroying the pickup object
+        yield return new WaitForSeconds(pickupAudioSource.clip.length);
+
+        // Implement any other behavior you want before destroying the pickup
+        Destroy(gameObject);
     }
 }
