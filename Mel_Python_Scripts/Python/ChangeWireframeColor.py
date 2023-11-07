708842416d22ded459d4a2452391b9a60435b9aa
Sean Anderson <nova7prospekt@gmail.com>
2023-11-07 14:11:59 -0700

    Translated Wireframe Color MEL script into Python script

---
+++ Mel_Python_Scripts/Python/ChangeWireframeColor.py
@@ -0,0 +1,25 @@
+import maya.cmds as cmds
+
+def wireframeColor(color):
+    # Get the current selection
+    selection = cmds.ls(selection=True)
+
+    if not selection:
+        cmds.error("Select an object(s) before running wireframeColor().")
+
+    if color < 0 or color > 31:
+        color = 0
+        cmds.warning("Color argument must be within the range of 0 to 31.")
+
+    for sel in selection:
+        shapes = cmds.listRelatives(sel, shapes=True)
+        print(f"Shapes for {sel}: {shapes}")  # Debugging line
+
+        for shape in shapes:
+            cmds.setAttr(shape + ".overrideEnabled", 1)
+            cmds.setAttr(shape + ".overrideColor", color)
+            print(f"Changed color for {shape} to {color}")  # Debugging line
+
+# Usage example:
+# Call the function and pass the desired color as an argument (0 to 31)
+wireframeColor(10)
